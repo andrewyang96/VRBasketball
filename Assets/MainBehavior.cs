9754507fb053ecb91e287e2fd48b5c603afad126
Yang <ayang14@illinois.edu>
2018-04-04 21:03:41 -0500

    Initial work

---
+++ Assets/MainBehavior.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainBehavior : MonoBehaviour
+{
+
+    public enum GameState
+    {
+        MAINMENU,
+        GAMESTART,
+        GAMEREADY,
+        GAMECOUNTDOWN,
+        GAMEOVER
+    }
+
+    public GameState s;
+    public float countUp = 0.0f;
+    public float endTime = 3.0f;
+    // Use this for initialization
+    void Start()
+    {
+        s = GameState.MAINMENU;
+        Debug.Log("start");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (OVRInput.Get(OVRInput.Button.Start) && (s == GameState.MAINMENU))
+        { //start button pressed
+            Debug.Log("mode chosen");
+            s = GameState.GAMEREADY;
+        }
+
+        //Debug.log(countdown);
+        if (s == GameState.GAMEREADY)
+        {
+            Debug.Log(countUp);
+            //animation
+            countUp += Time.deltaTime;
+            if (countUp >= 3.0f)
+            {
+                s = GameState.GAMECOUNTDOWN;
+                countUp = 0.0f;
+            }
+        }
+        else if (s == GameState.GAMECOUNTDOWN)
+        {
+            Debug.Log("game play");
+            Debug.Log(countUp);
+            countUp += Time.deltaTime;
+            if (countUp >= endTime)
+            {
+                s = GameState.GAMEOVER;
+                countUp = 0.0f;
+            }
+        }
+        else if (s == GameState.GAMEOVER)
+        {
+            Debug.Log("game over");
+            Debug.Log(countUp);
+            countUp += Time.deltaTime;
+            if (countUp >= 3.0f)
+            {
+                s = GameState.MAINMENU;
+                countUp = 0.0f;
+            }
+        }
+
+
+    }
+}
