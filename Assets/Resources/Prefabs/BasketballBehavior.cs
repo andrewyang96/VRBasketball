d3df180081ac7bf6bedd60d083d89763d345f56e
Yang <ayang14@illinois.edu>
2018-04-23 15:45:09 -0500

    Dust explosion when basketball disappears

--- Assets/Resources/Prefabs/BasketballBehavior.cs
+++ Assets/Resources/Prefabs/BasketballBehavior.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BasketballBehavior : MonoBehaviour {
+	GameObject explodingDust;
 	float destroyCountdown;
 	const float MAX_DESTROY_TIME = 2f;
 	const float MAGNITUDE_THRESHOLD = 1f;
@@ -12,6 +13,7 @@ public class BasketballBehavior : MonoBehaviour {
 	void Start () {
 		destroyCountdown = MAX_DESTROY_TIME;
 		wasThrown = false;
+		explodingDust = (GameObject)Resources.Load ("Prefabs/exploding_dust", typeof (GameObject));
 	}
 	
 	// Update is called once per frame
@@ -19,7 +21,9 @@ public class BasketballBehavior : MonoBehaviour {
 		Rigidbody rb = GetComponent<Rigidbody> ();
 		if (wasThrown && rb.velocity.magnitude < MAGNITUDE_THRESHOLD) {
 			if (destroyCountdown <= 0) {
+				// destroy self once enough time has elapsed & initiate exploding dust effect
 				Destroy (gameObject);
+				Instantiate (explodingDust, transform.position, transform.rotation);
 			}
 			destroyCountdown -= Time.deltaTime;
 			print (destroyCountdown);
