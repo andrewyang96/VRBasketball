f20f2dda7fe7b8c6db1aba90ec1fa98c9d88f8cb
Yang <ayang14@illinois.edu>
2018-04-27 16:15:19 -0500

    Adjust box collider in basketball net

--- Assets/Prefabs/basketball_net_and_board/BasketballNetBehavior.cs
+++ Assets/Prefabs/basketball_net_and_board/BasketballNetBehavior.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class BasketballNetBehavior : MonoBehaviour {
 	MainBehavior mainScript;
+	GameObject fireworksPrefab;
+	BoxCollider bc;
 
 	// Use this for initialization
 	void Start () {
 		mainScript = GameObject.Find ("Floor").GetComponent<MainBehavior> ();
+		fireworksPrefab = (GameObject)Resources.Load ("Prefabs/fireworks", typeof (GameObject));
+		bc = GetComponent<BoxCollider> ();
 	}
 	
 	// Update is called once per frame
@@ -20,6 +24,9 @@ public class BasketballNetBehavior : MonoBehaviour {
 		if (rb.velocity.y < 0) {
 			// if the ball is coming down
 			mainScript.incrementScore ();
+
+			// fireworks animation
+			Instantiate(fireworksPrefab, bc.transform.position, Quaternion.identity);
 		}
 	}
 }
