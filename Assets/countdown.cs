38a221bfa478133ea2ed603453126690723f946b
Damisch <bdamisc2@illinois.edu>
2018-04-27 13:44:03 -0500

    spaceballs and scoreboards

---
+++ Assets/countdown.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class countdown : MonoBehaviour {
+
+
+	public Text C;
+	public float timeLeft;
+
+	// Use this for initialization
+	void Start () {
+		//timeLeft = GameObject.Find("Floor").GetComponent<MainBehavior>().countUp;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeLeft = GameObject.Find("Floor").GetComponent<MainBehavior>().countUp;
+		C.text = "Time remaining: " + timeLeft.ToString("F2");
+	}
+}
---
+++ Assets/scoring.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scoring : MonoBehaviour {
+
+	public Text C;
+	public int score;
+
+	// Use this for initialization
+	void Start () {
+		//timeLeft = GameObject.Find("Floor").GetComponent<MainBehavior>().countUp;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		score = GameObject.Find("Floor").GetComponent<MainBehavior>().score;
+		score *= 2;
+		C.text = "Score:\n" + score.ToString();
+	}
+}
